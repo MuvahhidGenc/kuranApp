fb048c8462c18dbe3c194c02172d615cd3a10e44
muslimMan <omer.yyildirim@gmail.com>
2022-03-04 11:42:15 +0300

    Takipli Kuran Paragraf Haline Getirildi

--- lib/test/view/followquran_view.dart
+++ lib/test/view/followquran_view.dart
@@ -20,64 +20,98 @@ class _FollowQuranViewState extends State<FollowQuranView> {
     super.initState();
     //initAsyc();
   }
-
+  
   initAsyc(int page) async {
+    getText=null;
     getText =
         await _followQuranViewModel.getText(pageNo: page, kariId: "ar.alafasy");
     pagetext = "";
-    
-    getText!.map((e) {
+
+    /*getText!.map((e) {
       print(e.text);
-      pagetext +=" ﴿ "+ _followQuranViewModel.convertToArabicNumber(e.numberInSurah) +
-           " ﴾  " +
+      pagetext += " ﴿ " +
+          _followQuranViewModel.convertToArabicNumber(e.numberInSurah) +
+          " ﴾  " +
           e.text;
-    }).toList();
+    }).toList();*/
     setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-   var theme=SnippetExtanstion(context).theme;
+    var theme = SnippetExtanstion(context).theme;
     return Scaffold(
       appBar: AppBar(
-        title: Text("data",textAlign: TextAlign.left,),
+        title: Text(
+          "data",
+          textAlign: TextAlign.left,
+        ),
       ),
       body: PageView.builder(
-
         itemCount: 604,
         itemBuilder: (context, index) {
-           initAsyc(index+1);
+          initAsyc(index + 1);
           // ignore: avoid_unnecessary_containers
           return SafeArea(
-          /*  width: MediaQuery.of(context).size.width * 0.9,*/
-           /* decoration: BoxDecoration(
+            /*  width: MediaQuery.of(context).size.width * 0.9,*/
+            /* decoration: BoxDecoration(
               border: Border.all(
                 color: Colors.black,
               ),
             ),*/
             child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child:Container(
-        child: //Text(pagetext)
-         ListView.builder(
+              padding: const EdgeInsets.all(5.0),
+              child: Container(
+                /*decoration: BoxDecoration(
+                  color: theme.textTheme.bodyText1!.color
+                ),*/
+                child: ListView(
+                  children: [
+                    RichText(
+                      overflow: TextOverflow.clip,
+                      textScaleFactor: 1.1,
+                      textAlign: TextAlign.right,
+                      textDirection: TextDirection.rtl,
+                        text: TextSpan(
+                       style: TextStyle(fontSize: 18,/*color: theme.backgroundColor*//*backgroundColor: Colors.white*/),
+                      children: getText?.map((e) {
+                        return TextSpan(text: e.text.trim(), children: [
+                          TextSpan(
+                            text: " ﴿" +
+                                _followQuranViewModel
+                                    .convertToArabicNumber(e.numberInSurah) +
+                                "﴾ ",
+                            style: TextStyle(
+                                fontSize: 18,color:Colors.brown ),
+                          ),
+                        ]);
+                      }).toList(),
+                    )),
+                  ],
+                ),
+                //Text(pagetext)
+                /* ListView.builder(
                 shrinkWrap: true,
                 itemCount: getText?.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return  /*RichText(text: TextSpan(
-                    children: [
-                      TextSpan(text:_followQuranViewModel.convertToArabicNumber(
-                            getText?[index].numberInSurah)),
-                      TextSpan(text:getText?[index].text.trim() ),
-                    ]
-                  ));*/ListTile(
+                itemBuilder: (BuildContext context, int i) {*/
+                //return
+                /*RichText(
+                        text: TextSpan(children: [
+                 
+                  TextSpan(text: getText?[i].text.trim()+""),
+                   TextSpan(
+                    text: _followQuranViewModel
+                        .convertToArabicNumber(getText?[i].numberInSurah),
+                  ),
+                ])),*/ /*ListTile(
                     tileColor:theme.backgroundColor,
                         leading: CircleAvatar(
                         maxRadius: 12,
                         child: Text(_followQuranViewModel.convertToArabicNumber(
-                            getText?[index].numberInSurah)),
+                            getText?[i].numberInSurah)),
                       ),
                         title: Text(
-                          "  " + getText?[index].text.trim(),
+                          "  " + getText?[i].text.trim(),
                           //overflow: TextOverflow.ellipsis,
                           //  locale: Locale("UAE","971"),
                           //softWrap: false,
@@ -85,10 +119,10 @@ class _FollowQuranViewState extends State<FollowQuranView> {
                           
                           textAlign: TextAlign.right,
                         ),
-                      );
-                },
+                      );*/
+                // },
+                // ),
               ),
-            ),
             ),
           );
         },
